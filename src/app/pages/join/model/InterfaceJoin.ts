a48c4e07bc35a96bcc73a09cb0786f7fd34fc3d2
Daniel <daniel.gonzalez.guijarro@gmail.com>
2020-12-07 00:09:54 +0100

    primera prueba tras volver atrás

---
+++ src/app/pages/join/model/InterfaceJoin.ts
@@ -0,0 +1,27 @@
+export interface InterfaceJoin {
+
+    id: string;
+    name : string;
+    gender : string;
+    race : string;
+    weight : string[];
+    image : string;
+    intelligence : string;
+    strength: string;
+    speed: string;
+    durability: string;
+    power: string;
+    combat :string;
+    fullname: string;
+    alterEgo: string;
+    aliases: string[];
+    placeOfBirth : string;
+    alignment: string;
+    eyeColor: string;
+    hairColor : string;
+    occupation : string;
+    base : string;
+    groupAffili: string;
+
+
+}
